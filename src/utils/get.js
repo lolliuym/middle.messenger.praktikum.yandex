d7f41532492cf73644e98cbd0d72b2e161b51ca6
Valery Anestiadi <anest12mmm@gmail.com>
2022-07-14 21:02:45 +0300

    .

---
+++ src/utils/get.js
@@ -0,0 +1,10 @@
+﻿const obj = {
+  data: {
+    first: 'Vladislav',
+    second: true,
+  },
+};
+
+get(obj, 'data.first'); // 'Vladislav'
+get(obj, 'data.third'); // undefined
+get(obj, 'data.third', 'defaultValue'); // 'defaultValue' 
