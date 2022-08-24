ee83f5a13fada2beb76f862725fd864f347dbe95
Valery Anestiadi <anest12mmm@gmail.com>
2022-08-24 23:45:56 +0300

    .

--- src/components/labelCol/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
- import tpl from './tpl.hbs';
-
-
-Handlebars.registerPartial('LabelCol', tpl);
-
-export default (value, type, name) => {
-	return tpl({ value, type, name });
-}
--- src/components/labelRow/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
- import tpl from './tpl.hbs';
-
-
-Handlebars.registerPartial('LabelRow', tpl);
-
-export default (value, cls_label, cls_input, type, name, value_input) => {
-	return tpl({ value, cls_label, cls_input, type, name, value_input });
-}
